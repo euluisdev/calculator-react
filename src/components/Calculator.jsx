d0dc02adb2430554d3115a8fa15a58ab6ca48583
euluis-dev <fluisf00@gmail.com>
2024-04-01 19:33:36 -0300

    Box was imported in the Calculator.jsx file

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import './Calculator.css'
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 function Calculator() {
 
     return (
-        <Container>
-            <div className="wrapper" >
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>AC</button>
-                <button>+/-</button>
-                <button>%</button>
-                <button>/</button>
-                <button>X</button>
-                <button>-</button>
-                <button>+</button>
-                <button>=</button>
-            </div>
-        </Container>
+        <div>
+            <Box m={6} />
+            <Container maxWidth="xs" >
+                <div className="wrapper" >
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>AC</button>
+                    <button>+/-</button>
+                    <button>%</button>
+                    <button>/</button>
+                    <button>X</button>
+                    <button>-</button>
+                    <button>+</button>
+                    <button>=</button>
+                </div>
+            </Container>
+        </div>
     )
 }
 
