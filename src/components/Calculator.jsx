f2bb2cce13e20b554e1207e511a1cb4049a2705b
euluis-dev <fluisf00@gmail.com>
2024-04-01 17:22:05 -0300

    the buttons was created in Calculator.jsx file

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,9 +3,25 @@ import React from "react";
 function Calculator() {
 
     return (
-      <>
-        euluis-dev
-      </>
+        <div>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>AC</button>
+            <button>+/-</button>
+            <button>%</button>
+            <button>/</button>
+            <button>X</button>
+            <button>-</button>
+            <button>+</button>
+            <button>=</button>
+        </div>
     )
   }
   
