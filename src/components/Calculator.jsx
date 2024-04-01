bf1df18d049955122c0705c19e15bf47fdffe19a
euluis-dev <fluisf00@gmail.com>
2024-04-01 19:12:29 -0300

    the ui material file package was installed, and the Compnent was imported

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import './Calculator.css'
+import { Container } from "@mui/material";
 
 function Calculator() {
 
     return (
-        <div className="wrapper" >
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>AC</button>
-            <button>+/-</button>
-            <button>%</button>
-            <button>/</button>
-            <button>X</button>
-            <button>-</button>
-            <button>+</button>
-            <button>=</button>
-        </div>
+        <Container>
+            <div className="wrapper" >
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>AC</button>
+                <button>+/-</button>
+                <button>%</button>
+                <button>/</button>
+                <button>X</button>
+                <button>-</button>
+                <button>+</button>
+                <button>=</button>
+            </div>
+        </Container>
     )
-  }
-  
-  export default Calculator;
+}
+
+export default Calculator;
