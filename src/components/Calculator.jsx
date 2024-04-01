73489bc0e6c21d98477f9807941e83f655936071
euluis-dev <fluisf00@gmail.com>
2024-04-01 19:48:03 -0300

    the orange and gray class was created, to style the buttons

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -12,21 +12,21 @@ function Calculator() {
                     <button>AC</button>
                     <button>+/-</button>
                     <button>%</button>
-                    <button>/</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>*</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>-</button>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>+</button>
-                    <button>0</button>
-                    <button>,</button>
+                    <button className="orange">/</button>
+                    <button className="gray">7</button>
+                    <button className="gray">8</button>
+                    <button className="gray">9</button>
+                    <button className="orange">*</button>
+                    <button className="gray">4</button>
+                    <button className="gray">5</button>
+                    <button className="gray">6</button>
+                    <button className="orange">-</button>
+                    <button className="gray">1</button>
+                    <button className="gray">2</button>
+                    <button className="gray">3</button>
+                    <button className="orange">+</button>
+                    <button className="gray">0</button>
+                    <button className="gray">,</button>
                     <button>=</button>
                 </div>
             </Container>
