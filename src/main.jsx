cd2b5d0db1e3d570f531371c063d64871612cce6
euluis-dev <fluisf00@gmail.com>
2024-04-01 16:55:05 -0300

    the main.jsx file was configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
