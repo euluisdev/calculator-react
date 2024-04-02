747a69b8a6ee285a7248e0aa588a19a04306be69
euluis-dev <fluisf00@gmail.com>
2024-04-02 15:46:43 -0300

    updateScreen function was created and onClick function was added to each number

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,33 +3,38 @@ import './Calculator.css'
 import { Box, Container } from "@mui/material";
 
 function Calculator() {
-    const [current, setCurrent] = useState(0);
+    const [num, setNum] = useState('0');
+
+    function updateScreen (e) {
+        const inputValue = e.target.innerText;
+        setNum(prevNum => prevNum === '0' ? inputValue : prevNum + inputValue);
+    }
 
     return (
         <div>
             <Box m={6} />
             <Container maxWidth="xs">
                 <div className="wrapper">
-                    <h1 className="resOperation">{current}</h1>
+                    <h1 className="resOperation">{num}</h1>
                     <Box m={9} />
                     <button>AC</button>
                     <button>+/-</button>
                     <button>%</button>
                     <button className="orange">/</button>
-                    <button className="gray">7</button>
-                    <button className="gray">8</button>
-                    <button className="gray">9</button>
-                    <button className="orange">*</button>
-                    <button className="gray">4</button>
-                    <button className="gray">5</button>
-                    <button className="gray">6</button>
+                    <button className="gray" onClick={updateScreen}>7</button>
+                    <button className="gray" onClick={updateScreen}>8</button>
+                    <button className="gray" onClick={updateScreen}>9</button>
+                    <button className="orange" onClick={updateScreen}>*</button>
+                    <button className="gray" onClick={updateScreen}>4</button>
+                    <button className="gray" onClick={updateScreen}>5</button>
+                    <button className="gray" onClick={updateScreen}>6</button>
                     <button className="orange">-</button>
-                    <button className="gray">1</button>
-                    <button className="gray">2</button>
-                    <button className="gray">3</button>
+                    <button className="gray" onClick={updateScreen}>1</button>
+                    <button className="gray" onClick={updateScreen}>2</button>
+                    <button className="gray" onClick={updateScreen}>3</button>
                     <button className="orange">+</button>
-                    <button className="gray wide">0</button>
-                    <button className="gray">,</button>
+                    <button className="gray wide" onClick={updateScreen}>0</button>
+                    <button className="gray" onClick={updateScreen}>,</button>
                     <button className="orange">=</button>
                 </div>
             </Container>
